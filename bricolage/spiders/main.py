7c9f1127c791a1dec1e501b8b294d8a27a21e11a
Bogomil Filipov <filipov.bogomil@gmail.com>
2019-01-04 00:47:23 +0200

    iterating over the list pages

--- bricolage/spiders/main.py
+++ bricolage/spiders/main.py
@@ -5,7 +5,18 @@ import scrapy
 class MainSpider(scrapy.Spider):
     name = 'main'
     allowed_domains = ['mr-bricolage.bg']
-    start_urls = ['mr-bricolage.bg/bg/Каталог/Инструменти/Авто-и-велоаксесоари/Велоаксесоари/c/006008012']
+    start_urls = ['https://mr-bricolage.bg/bg/Каталог/Инструменти/Авто-и-велоаксесоари/Велоаксесоари/c/006008012']
 
     def parse(self, response):
-        pass
+        for item in response.xpath("//div[@class='product']"):
+            yield {
+                'title': item.xpath(".//div[@class='title']/a/text()").extract_first(),
+                'price': item.xpath(".//div[@class='price']/text()").extract_first(),
+                'image': item.xpath(".//div[@class='image']/a/img/@src").extract_first(),
+            }
+
+        next_page = response.xpath("//li[@class='pagination-next']").extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
